5c289705af2d088bd12cdff949e33263719c28ec
Ayesha2523 <111120173+Ayesha2523@users.noreply.github.com>
2023-07-05 12:54:06 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string users[4] = {"eliza","musa","romaisa","sawleha"};
+    
+    for(int a=0;a<4;a++){
+        
+   cout<<users[a] <<"\n" ;}
+    
+    
+    
+    
+}
